7dfb6ccd0aab2ba10c830cdf75e6845fee3d67ed
Jarom Loveridge <jaroml@gradecam.com>
2018-04-14 14:54:43 -0600

    Use a better boolean conversion scheme.

--- lib/castable.ts
+++ lib/castable.ts
@@ -58,9 +58,21 @@ function convert(value: any, type: any, propertyKey: string, depth: number, obj?
 }
 
 /**
- * Converts various values to a boolean.
+ * Converts a value to a boolean.
  * @param val value to be converted
  */
 export function toBool(val: any): boolean {
-  return typeof val === 'number' ? val !== 0 : /^(t|true|y|yes|on)/.test(String(val).trim().toLowerCase());
+  switch (typeof val) {
+      case 'boolean':
+          return val;
+      case 'number':
+          // NaN and 0 should both be false, anything else true
+          return !!val;
+      case 'object':
+          return val ? Object.keys(val).length > 0 : false;
+      case 'string':
+          return /^(t|true|y|yes|on|1)$/i.test(val.trim());
+      default:
+          return false;
+  }
 }
