e0cc45dcd31e49a670d6004e3b4b1b0dad1f627e
Jarom Loveridge <jaroml@gradecam.com>
2018-04-14 14:52:21 -0600

    Make boolean conversion cleaner.

--- lib/castable.ts
+++ lib/castable.ts
@@ -50,9 +50,21 @@ export class Castable {
 }
 
 /**
- * Converts various values to a boolean.
+ * Converts a value to a boolean.
  * @param val value to be converted
  */
 export function toBool(val: any): boolean {
-  return typeof val === 'number' ? val !== 0 : /^(t|true|y|yes)/.test(String(val))
+  switch (typeof val) {
+      case 'boolean':
+          return val;
+      case 'number':
+          // NaN and 0 should both be false, anything else true
+          return !!val;
+      case 'object':
+          return val ? Object.keys(val).length > 0 : false;
+      case 'string':
+          return /^(t|true|y|yes|on|1)$/i.test(val.trim());
+      default:
+          return false;
+  }
 }
